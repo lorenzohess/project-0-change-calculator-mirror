9aae3a6dcf56523f44cf1a0497e19d148e87688f
Lorenzo Hess <lorenzohess@tutanota.com>
2025-01-19 18:15:22 -0500

    Initial commit

---
+++ change_calculator.c
@@ -0,0 +1,8 @@
+// Return 1 if paid >= total, else 0.
+int check_amount(float total, float paid);
+
+// Print quantity of denominations needed to give specified change.
+// Uses denominations: $5, $1, quarter, dime, nickel, penny.
+void print_formatted(float change);
+
+int main(void) { return 0; }
