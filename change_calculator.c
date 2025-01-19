23570832450fd9e482db1dfaf311d25b58929ee1
Lorenzo Hess <lorenzohess@tutanota.com>
2025-01-19 18:26:15 -0500

    Add docstrings

--- change_calculator.c
+++ change_calculator.c
@@ -1,8 +1,24 @@
-// Return 1 if paid >= total, else 0.
+/*******************************************************************************
+ * int check_amount(float total, float paid)
+ * Brief description of the function
+ * Parameters:
+ *     total: the purchase amount
+ *     paid: the amount the customer paid
+ * Returns:
+ *     1 if paid >= total, else 0
+ * Notes:
+ * Sources:
+ ******************************************************************************/
 int check_amount(float total, float paid);
 
-// Print quantity of denominations needed to give specified change.
-// Uses denominations: $5, $1, quarter, dime, nickel, penny.
+/*******************************************************************************
+ * void print_formatted(float change)
+ * Print quantity of denominations needed to give specified change.
+ * Parameters:
+ *     change: the change to print out
+ * Notes: Uses denominations: $5, $1, quarter, dime, nickel, penny.
+ * Sources:
+ ******************************************************************************/
 void print_formatted(float change);
 
 int main(void) { return 0; }
