bfcff09bfdedc3dcca438ad0fd7d246059db5aa2
Lorenzo Hess <lorenzohess@tutanota.com>
2025-01-19 19:17:43 -0500

    Implement user input functions

--- change_calculator.c
+++ change_calculator.c
@@ -1,13 +1,43 @@
+#include <stdio.h>
+
+/*******************************************************************************
+ * float get_purchase_total_amount(void)
+ * Get the purchase amount from the user.
+ * Returns:
+ *     the purchase amount
+ ******************************************************************************/
+float get_purchase_total_amount(void) {
+  float total;
+
+  puts("Input purchase total");
+  scanf("%f", &total);
+
+  return total;
+}
+
+/*******************************************************************************
+ * float get_paid_amount(void)
+ * Get the amount paid by the user.
+ * Returns:
+ *     paid: the amount paid
+ ******************************************************************************/
+float get_paid_amount(void) {
+  float paid;
+
+  puts("Input amount paid");
+  scanf("%f", &paid);
+
+  return paid;
+}
+
 /*******************************************************************************
  * int check_amount(float total, float paid)
- * Brief description of the function
+ * Ensure paid >= total.
  * Parameters:
  *     total: the purchase amount
  *     paid: the amount the customer paid
  * Returns:
  *     1 if paid >= total, else 0
- * Notes:
- * Sources:
  ******************************************************************************/
 int check_amount(float total, float paid);
 
@@ -17,8 +47,14 @@ int check_amount(float total, float paid);
  * Parameters:
  *     change: the change to print out
  * Notes: Uses denominations: $5, $1, quarter, dime, nickel, penny.
- * Sources:
  ******************************************************************************/
 void print_formatted(float change);
 
-int main(void) { return 0; }
+int main(void) {
+  float total = get_purchase_total_amount();
+  printf("Total: %f\n", total);
+
+  float paid = get_paid_amount();
+  printf("Total: %f\n", paid);
+  return 0;
+}
